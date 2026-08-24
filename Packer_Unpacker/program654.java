a1429a8ed3dab52ed06197a101ab775dbb936a68
Abhijeet Gorale <abhigorale99@gmail.com>
2026-08-24 22:28:09 +0530

    Add files via upload

---
+++ Packer_Unpacker/program654.java
@@ -0,0 +1,13 @@
+import java.io.*;        // input output
+
+class program654
+{
+    public static void main(String A[])
+    {
+        File fobj = new File("Demo.txt") ;
+        
+        fobj.createNewFile();    // create new file  checked exception error
+
+
+    }
+}
