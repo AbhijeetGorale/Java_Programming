dbc1705c84c2794ccbc84ff0a39f36cf7e639e35
Abhijeet Gorale <abhigorale99@gmail.com>
2026-08-26 23:50:08 +0530

    Add files via upload

---
+++ Packer_Unpacker/program663.java
@@ -0,0 +1,15 @@
+//file writter write String 
+//file outputStream write binary
+
+import java.io.*; 
+import java.util.*;  
+
+class program663                                     
+{
+    public static void main(String A[]) throws IOException
+    {
+        FileWriter fwobj = new FileWriter("Demo.txt");
+        
+        fwobj.write("Jay Ganesh...");
+    }
+} 
