28eb189ac9291c0b68345d6c84b2f0e403008a54
Abhijeet Gorale <abhigorale99@gmail.com>
2026-08-27 13:39:27 +0530

    Add files via upload

---
+++ Packer_Unpacker/program666.java
@@ -0,0 +1,29 @@
+//file writter write String 
+//file outputStream write binary
+
+import java.io.*; 
+import java.util.*;  
+
+class program666                        
+{
+    public static void main(String A[])
+    {
+        FileReader frobj = null ;
+        try
+        {
+            frobj = new FileReader("Demo.txt");
+            
+            System.out.println(frobj.read());
+
+        }
+        catch(IOException iobj)
+        {
+            System.out.println("iobj");
+        }
+        finally
+        {
+              
+        }
+        
+    }
+} 
