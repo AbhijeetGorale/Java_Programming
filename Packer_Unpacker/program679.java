433ab50c33f876fc423b6312bab660694b7faf64
Abhijeet Gorale <abhigorale99@gmail.com>
2026-08-29 21:13:54 +0530

    Add files via upload

---
+++ Packer_Unpacker/program679.java
@@ -0,0 +1,36 @@
+import java.io.*;      // file outputString use to write in byte
+import java.util.*;  
+
+class program679     
+{
+    public static void main(String A[]) throws Exception
+    {
+        Scanner sobj = new Scanner(System.in);
+        String Fname = null;
+        File fobj = null ;
+
+        FileInputStream fiobj =  null ;
+
+        System.out.println("Enter file name :");
+        Fname = sobj.nextLine();
+
+        fobj = new File(Fname);
+
+        fiobj  = new FileInputStream(fobj);
+
+
+        byte Arr [] = new byte[50];
+
+        if(fobj.exists())
+        {
+            fiobj.read(Arr); 
+            String str =  new String(Arr);
+            System.out.println(new String(Arr));     
+        }
+        else
+        {
+            System.out.println("There is no such file");
+        }
+        
+    }
+} 
