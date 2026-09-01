b47b4318ead1fe329e733dcd8c474051238ac10d
Abhijeet Gorale <abhigorale99@gmail.com>
2026-09-01 23:18:51 +0530

    Add files via upload

---
+++ Packer_Unpacker/program689.java
@@ -0,0 +1,35 @@
+import java.io.*;      // folder
+import java.util.*;  
+
+class program689
+{
+    public static void main(String A[]) throws Exception
+    {
+        Scanner sobj = new Scanner(System.in);
+        String FolderName = null ;
+
+        System.out.println("Enter Folder Name: ");
+        FolderName = sobj.nextLine();
+
+        File fobj = new File(FolderName);
+
+        if((fobj.exists()) && (fobj.isDirectory()))
+        {
+            System.out.println("Folder exists");
+
+            File fArr[] = fobj.listFiles();     //os.walk
+
+            System.out.println("Number of Files : "+fArr.length);
+
+            for(int i = 0 ; i < fArr.length ; i++)
+            {
+                System.out.println(fArr[i].getName());
+                System.out.println(fArr[i].length());  // size of files         
+            }
+        }
+        else
+        {
+            System.out.println("There is no such folder");
+        }
+    }
+} 
