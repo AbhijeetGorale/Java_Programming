164162e5e1b4dab429a500dbe20d8db20444d3fd
abhigorale3297 <abhigorale99@gmail.com>
2026-09-12 11:53:59 +0530

    Added 5 files for feature Packer_unpacker

---
+++ Packer_Unpacker/program700.java
@@ -0,0 +1,14 @@
+class program700
+{
+    public static void main(String A[]) 
+    {
+       String header = "Marvellous" ;
+
+       System.out.println(header);
+       header.replaceAll("l","L");  // issue
+
+
+       System.out.println(header);
+    }
+
+}
