dcd8594ce8cb0fdeb615f88a1c62d66dde500606
abhigorale3297 <abhigorale99@gmail.com>
2026-09-12 22:49:15 +0530

    Updated trim

---
+++ Packer_Unpacker/program706.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class program706
+{
+    public static void main(String A[]) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        String str = null;
+
+        System.out.println("Ener String :");
+        str = sobj.nextLine();
+
+        System.out.println("String is :"+str+" having length :"+str.length());
+
+        str = str.trim();
+        System.out.println("String is :"+str+" having length :"+str.length());
+
+
+       
+    }
+
+}
