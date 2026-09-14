590b5d45ffeb63d0b66e8d8b8c4dbb2a2b50d75f
abhigorale3297 <abhigorale99@gmail.com>
2026-09-14 12:17:40 +0530

    New Files

---
+++ Packer_Unpacker/program710.java
@@ -0,0 +1,12 @@
+class program710
+{
+    public static void main(String A[]) 
+    {
+      String header = "India  is  my  country" ;
+
+       String Arr[] = header.split(" ");   // if space found counts  as 1
+
+       System.out.println("Number of words are :"+Arr.length);
+    }
+
+}
